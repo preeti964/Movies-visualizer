f901fa54e91bca5c8908f7a6a34f8b0f30b72669
Preeti Maurya <mauryapreeti964@gmail.com>
2022-12-10 15:03:21 +0530

    Added sort

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,11 +2,53 @@ import { moviesData } from '../data/movieslist';
 import MovieCard from './MovieCard';
 import { CiSearch } from 'react-icons/ci';
 import './movielist.css';
+import { useState } from 'react';
+import SideBar from './SideBar';
 
 const MovieList = () => {
+
+    const [sortrating, setsortrating] = useState(moviesData);
+    const [movieGen, setMovieGen] = useState('');
+    const [showData, setShowData] = useState(true);
+    const sortImdb = () => {
+        let arr1 = [];
+        var arr = sortrating.sort((a, b) => {
+            // if(abc=="imdbRating")
+            return a.imdbRating - b.imdbRating;
+        });
+        for (let i of arr) {
+            arr1.push(i);
+        }
+        setsortrating(arr1);
+    };
+
+    const sortYear = () => {
+        let arr1 = [];
+        var arr = sortrating.sort((a, b) => {
+            return a.year - b.year;
+        });
+        for (let i of arr) {
+            arr1.push(i);
+        }
+        setsortrating(arr1);
+    }
+
+    const actionHandler = (event) => {
+        console.log(event.target.innerText);
+
+        setMovieGen(event.target.innerText);
+        if (event.target.innerText === 'All')
+            setShowData(true);
+        else
+            setShowData(false);
+    };
+
+    const filter_data = sortrating.filter((xyz) => { return xyz.genres.includes(movieGen) });
     return (
         <div className="main-dashboard-container">
-            <div className='left-dashboard-container'>BM</div>
+            <div className='left-dashboard-container'>
+                <SideBar/>
+            </div>
             <div className='right-dashboard-container'>
                 <div className='search-container'>
                     <CiSearch className='search-icon' />
@@ -15,13 +57,21 @@ const MovieList = () => {
 
                 <div className='parent-container'>
                     <div className='left-container'>
-                        <p style={{color:'red',marginTop:'10px'}}>GENRES</p>
-                        <hr/>
-                        <ul className='genres'>
-                            <li>All</li>
-                            <li>Action</li>
+                        <p style={{ color: 'white', marginTop: '10px' }}>GENRES</p>
+                        <hr />
+                        <ul onClick={actionHandler} className='genres'>
+                            <li >All</li>
+                            <li >Action</li>
                             <li>Adventure</li>
                             <li>Biography</li>
+                            <li>Comedy</li>
+                            <li>Crime</li>
+                            <li>Drama</li>
+                            <li>History</li>
+                            <li>Mystery</li>
+                            <li>Scifi</li>
+                            <li>Sport</li>
+                            <li>Thriller</li>
                         </ul>
 
                     </div>
@@ -29,16 +79,38 @@ const MovieList = () => {
                     <div className='right-container'>
                         <div className='sort-container'>
                             <span>24 Movies</span>
-                            <span>SORT BY:
-                            
-                            <select className="" aria-label="Default select example">
-                                    <option>Latest</option>
-                                    <option value="1">Imdb Rating</option>
-                                </select></span>
+                            <div className="dropdown m-1 left " >
+                                <button
+                                    className="btn btn-secondary dropdown-toggle sort-by"
+                                    type="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Sort by
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <a className="dropdown-item" onClick={sortImdb}>
+                                            imdb Rating
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" onClick={sortYear}>
+                                            Year
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
 
                         <div className='movie-container'>
-                            {moviesData.map((movie, index) => (
+                            {
+                                showData && sortrating.map((movie, index) => (
+                                    <MovieCard movie={movie} key={index} />
+                                ))
+                            }
+
+                            {!showData && filter_data.map((movie, index) => (
                                 <MovieCard movie={movie} key={index} />
                             ))}
                         </div>
---
+++ src/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './SideBar.css'
+export default function SideBar() {
+  return (
+      <div className="sideLine">
+        <ul>
+          <li><i class="fa fa-eye" aria-hidden="true"></i></li>
+          <li><i class="fa fa-desktop" aria-hidden="true"></i></li>
+          <li><i class="fa fa-home" aria-hidden="true"></i></li>
+          
+          <li><i class="fa fa-folder-open" aria-hidden="true"></i></li> 
+         
+        </ul>
+        </div>
+  );
+}
